8fcc6d4e39cf5a5a2142dde506ea5648ffa58e71
ram singh <ramsg211@gmail.com>
2024-03-29 16:34:42 +0530

    Login Module Files

---
+++ LoginModule/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using LoginModule.Models;
+
+
+namespace LoginModule.Controllers
+{
+    public class AccountController : Controller
+    {
+        CustomerDbEntities entities = new CustomerDbEntities();
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel userLogin)
+        {
+            bool custExist = entities.customers.Any(x => x.Email == userLogin.Email && x.Password == userLogin.Password);
+            customer cus = entities.customers.FirstOrDefault(x => x.Email == userLogin.Email && x.Password == userLogin.Password);
+
+            if (custExist)
+            {
+                FormsAuthentication.SetAuthCookie(cus.Email, false);
+               return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Username or password is wrong");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(customer customerDetails)
+        {
+            entities.customers.Add(customerDetails);
+            entities.SaveChanges();
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("demo@gmail.com");
+            message.Subject = "User Registration";
+            message.To.Add(new MailAddress("demo22@gmail.com"));
+            message.Body = "<html><body>Registration Successfull</body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("demo@gmail.com", "xxxxx"),
+                EnableSsl=true,
+            };
+            smtpClient.Send(message);
+            return RedirectToAction("Login");
+            
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+            //return View();
+        }
+    }
+}
---
+++ LoginModule/Controllers/CustomersController.cs
@@ -0,0 +1,60 @@
+﻿using LoginModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginModule.Controllers
+{
+    public class CustomersController : Controller
+    {
+        CustomerDbEntities entities = new CustomerDbEntities();
+        // GET: Customers
+        public async Task<ActionResult> CustomerList()
+        {
+            var res = entities.customers.ToList();
+            return View(res);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var data = entities.customers.Where(x => x.Id == id).FirstOrDefault();
+            return View(data);
+          
+        }
+
+     
+
+        [HttpPost]
+        public ActionResult Edit(customer obj)
+        {
+            var data = entities.customers.Where(x => x.Id == obj.Id).FirstOrDefault();
+            if (data != null)
+            {
+                data.Id = obj.Id;
+                data.FirstName = obj.FirstName;
+                data.LastName = obj.LastName;
+                data.Email = obj.Email;
+                data.PhoneNumber = obj.PhoneNumber;
+                data.Password = obj.Password;
+                data.LoginUser = obj.LoginUser;
+                entities.SaveChanges();
+            }
+
+            return RedirectToAction("CustomerList");
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var res = entities.customers.Where(x => x.Id == id).First();
+            entities.customers.Remove(res);
+            entities.SaveChanges();
+
+            var list = entities.customers.ToList();
+            return View("CustomerList", list);
+        }
+    }
+}
